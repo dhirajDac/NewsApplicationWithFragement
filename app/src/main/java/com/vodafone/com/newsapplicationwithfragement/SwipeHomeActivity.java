d951a512df2f7e0a77253ac81a5d2aa4225235c7
dhirajDac <dhiraj.sk@gmail.com>
2016-08-06 15:19:39 +0530

    How to save preference

--- app/src/main/java/com/vodafone/com/newsapplicationwithfragement/SwipeHomeActivity.java
+++ app/src/main/java/com/vodafone/com/newsapplicationwithfragement/SwipeHomeActivity.java
@@ -1,11 +1,14 @@
 package com.vodafone.com.newsapplicationwithfragement;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.vodafone.com.myapplication.R;
@@ -15,6 +18,7 @@ import com.vodafone.com.newsapplicationwithfragement.objects.NewsApiSourceRefere
 import com.vodafone.com.newsapplicationwithfragement.objects.Source;
 
 import java.util.List;
+import java.util.Set;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,7 +28,7 @@ public class SwipeHomeActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_home);
         viewPager=(ViewPager)findViewById(R.id.activity_swipe_view_pager);
@@ -33,17 +37,52 @@ public class SwipeHomeActivity extends AppCompatActivity {
         sourceReference.enqueue(new Callback<NewsApiSourceReference>() {
             @Override
             public void onResponse(Call<NewsApiSourceReference> call, Response<NewsApiSourceReference> response) {
-                List<Source> allSource=response.body().getSources();
+                List<Source> allSource = response.body().getSources();
                 CommonUsage.setAllNewsSources(allSource);
-                ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getSupportFragmentManager(),allSource);
+
+                viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                    @Override
+                    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                      //  Log.i(TAG,"OnPageScroll "+position);
+
+                    }
+
+                    @Override
+                    public void onPageSelected(int position) {
+                       // Log.i(TAG,"OnPageSelected "+position);
+                        saveInPreference(position);
+
+                    }
+
+                    @Override
+                    public void onPageScrollStateChanged(int state) {
+                       // Log.i(TAG,"OnPageScrollStateChanged "+position);
+
+                    }
+                });
+                ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), allSource);
                 viewPager.setAdapter(viewPagerAdapter);
+                viewPager.setOffscreenPageLimit(8);
+
+                SharedPreferences sharedPreferences=PreferenceManager.getDefaultSharedPreferences(SwipeHomeActivity.this);
+                int position=sharedPreferences.getInt(KEY_POSITION,DEFAULT_POSITION);
+                if(position!=0)
+                {
+                    viewPager.setCurrentItem(position);
+                }
             }
+
+
 
             @Override
             public void onFailure(Call<NewsApiSourceReference> call, Throwable t) {
                 Toast.makeText(SwipeHomeActivity.this, "Error in Swip HomeAPICall", Toast.LENGTH_SHORT).show();
             }
         });
+
+
+
+
     }
 
     public class ViewPagerAdapter extends FragmentPagerAdapter
@@ -64,5 +103,22 @@ public class SwipeHomeActivity extends AppCompatActivity {
         public int getCount() {
             return (null == allSources) ? 0 : allSources.size();
         }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String title=CommonUsage.getAllNewsSources().get(position).getName();
+            return title;
+        }
     }
+
+    public static final String KEY_POSITION="storedposition";
+    public static final int DEFAULT_POSITION=0;
+    public void saveInPreference(int position)
+    {
+        SharedPreferences sharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putInt(KEY_POSITION,position);
+        editor.commit();
+    }
+
 }
